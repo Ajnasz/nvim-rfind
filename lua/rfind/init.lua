0ab61f9128bf1b9b985102adb2672899497ddaa2
Lajos Koszti <ajnasz@ajnasz.hu>
2023-10-05 07:41:01 +0200

    compile fnl to lua

--- lua/rfind/init.lua
+++ lua/rfind/init.lua
@@ -17,18 +17,18 @@ local function get_visual_end_line_num()
     return vim.fn.line("'>")
   end
 end
-local function get_search_command(start_pos, end_pos)
+local function get_find_command(start_pos, end_pos)
   return string.format("/\\%%>%dl\\%%<%dl", (start_pos - 1), (end_pos + 1))
 end
 local function get_feedkeys(start_pos, end_pos)
-  return string.format("%s%s", vim.api.nvim_replace_termcodes("<esc>", true, false, true), get_search_command(start_pos, end_pos))
+  return string.format("%s%s", vim.api.nvim_replace_termcodes("<esc>", true, false, true), get_find_command(start_pos, end_pos))
 end
-local function search_in_range(start_pos, end_pos)
+local function find_in_range(start_pos, end_pos)
   return vim.api.nvim_feedkeys(get_feedkeys(start_pos, end_pos), "ni", false)
 end
-local function search_in_visual()
+local function find_in_visual()
   local start_pos = get_visual_start_line_num()
   local end_pos = get_visual_end_line_num()
-  return search_in_range(math.min(start_pos, end_pos), math.max(start_pos, end_pos))
+  return find_in_range(math.min(start_pos, end_pos), math.max(start_pos, end_pos))
 end
-return {visual = search_in_visual, range = search_in_range}
+return {visual = find_in_visual, range = find_in_range}
